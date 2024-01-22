2703b49e2867de91952fc6b23d00cc884b8653d8
gavit <gavit21@iiserb.ac.in>
2024-01-22 17:57:39 +0530

    Clear function erarsed

--- LiDAR_Stereo_snapshots.m
+++ LiDAR_Stereo_snapshots.m
@@ -1,12 +1,17 @@
-clear all;close all;clc;
+clear all;
+close all;
+clc;
 
 % Open the ZED
 zed = webcam('ZED 2i');
-%LiDAR Initialization
+
+% LiDAR Initialization
 lidar = velodynelidar('VLP16');
 preview(lidar)
+
 % Set video resolution
 zed.Resolution = zed.AvailableResolutions{1};
+
 % Get image size
 [height, width, channels] = size(snapshot(zed));
 
@@ -14,47 +19,51 @@ zed.Resolution = zed.AvailableResolutions{1};
 f = figure('name','ZED camera','keypressfcn','close','windowstyle','modal');
 ok = 1;
 counter = 1;
+
 while ok
-      % Capture the current image
-      img = snapshot(zed);
-        
-      
-      % Split the side by side image image into two images
-      image_left = img(:, 1 : width/2, :);
-      image_right = img(:, width/2 +1: width, :);
-      
-      % Save the image on user prompt
-      X = input("Enter keystroke: ", "s");
-      if (X == "y")
+    % Capture the current image
+    img = snapshot(zed);
+
+    % Split the side by side image image into two images
+    image_left = img(:, 1 : width/2, :);
+    image_right = img(:, width/2 + 1 : width, :);
+
+    % Start LiDAR Point Cloud acquisition
+    start(lidar);
+    [frame, timestamp] = read(lidar, 1);
+
+    % Stop LiDAR Point Cloud acquisition
+    stop(lidar);
+
+    % Save the images and LiDAR data on user prompt
+    X = input("Enter keystroke: ", "s");
+    if (X == "y")
         % Saving the Split Stereo Camera Images
         f_name_l = append("img_left",tostring(counter),".jpg");
         f_name_r = append("img_right",tostring(counter),".jpg");
         imwrite(image_left, f_name_l);
         imwrite(image_right, f_name_r);
+
+        % Saving LiDAR data
+        lidar_data_filename = append("lidar_data", tostring(counter), ".mat");
+        save(lidar_data_filename, 'frame', 'timestamp');
+
         counter = counter + 1;
-        % Start LiDAR Point Cloud acquisition
-        start(lidar);        
-        [frame,timestamp] = read(lidar,1);
-        stop(lidar)
-        save lidardata.mat frames timestamps
-        clear lidar
+    end
 
+    % Display the left and right images
+    subplot(1,2,1);
+    imshow(image_left);
+    title('Image Left');
+    subplot(1,2,2);
+    imshow(image_right);
+    title('Image Right');
+    drawnow;
 
+    % Check for interrupts
+    ok = ishandle(f);
+end
 
-      end
-
-      % Display the left and right images
-      subplot(1,2,1);
-      imshow(image_left);
-      title('Image Left');
-      subplot(1,2,2);
-      imshow(image_right);
-      title('Image Right');
-      drawnow;
-
-      % Check for interrupts
-      ok = ishandle(f);
-  end
-
-  % close the camera instance
-  clear cam
+% Close the camera and LiDAR instances
+clear zed;
+clear lidar;
